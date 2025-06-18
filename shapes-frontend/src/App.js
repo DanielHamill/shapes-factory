09e153494d8dc74164e7cfd0d69d6e58638411f4
Daniel Hamill <danielhamill00@gmail.com>
2025-06-17 21:47:41 -0400

    deploy to github pages

--- shapes-frontend/src/App.js
+++ shapes-frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import * as React from "react";
 import DrawingCanvas from './components/DrawingCanvas';
+import { Routes, Route } from 'react-router-dom';
+
+// function Home() {
+//   return (
+//     <div>
+//       <h1>AI Shapes Factory</h1>
+//       <DrawingCanvas />
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Routes>
+//         <Route exact path="/" element={<Home />} />
+//     </Routes>
+//   );
+// }
+
 
 function App() {
   return (
     <div>
-      <h1>AI Shapes Factory</h1>
-      <DrawingCanvas />
+       <h1>AI Shapes Factory</h1>
+       <DrawingCanvas />
     </div>
   );
 }
--- shapes-frontend/src/index.js
+++ shapes-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+// root.render((
+//    <HashRouter basename={process.env.PUBLIC_URL}>
+//      <App />
+//    </HashRouter>
+// ))  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
